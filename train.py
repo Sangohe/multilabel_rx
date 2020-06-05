258e750aa448c4cce8abf72e345ae87b370f141f
Sangohe <santiagogomez.h@outlook.com>
2020-06-05 15:44:19 -0500

    Added evaluation function to train.py

--- train.py
+++ train.py
@@ -11,13 +11,11 @@ import dataset
 import callback
 
 
-def train_single_network(result_subdir=None, epochs=5, initial_lr=1e-3, verbose=1):
+def train_single_network(epochs=5, initial_lr=1e-3, verbose=1):
     """Trains a default tf.keras network only with the parent nodes from CheXpert"""
 
-    # Make sure the result directory exists and is not none, raise exception if any
-    # of the conditions are not met
-    if result_subdir is None or not os.path.exists(result_subdir):
-        raise Exception("Please make sure to point a directory to save your progress")
+    # Create result subdirectory to store the experiment results
+    result_subdir = utils.create_result_subdir(config.result_dir, config.desc)
 
     # Read dataset
     if config.train_record is not None:
@@ -115,6 +113,96 @@ def train_single_network(result_subdir=None, epochs=5, initial_lr=1e-3, verbose=
         print("Error. Could not save metric's plot")
 
 
+def evaluate_single_network(
+    run_id, test_record=None, class_names=None, metrics=None, log=None
+):
+    """
+    1. Locate the result_subdir using the run_id
+    2. Load the test dataset using test_record. Make sure to apply the 
+    same transformations read config.txt file in the result_subdir
+    3. Feedforward the test dataset and compare the predictions to 
+    the ground truth labels
+    4. Create a dictionary for the metrics and save it as pickle
+    """
+    import pickle
+    import tensorflow_addons as tfa
+    from sklearn.metrics import roc_auc_score
+
+    metrics_class_names = {
+        "acc": tf.keras.metrics.BinaryAccuracy(name="accuracy"),
+        "precision": tf.keras.metrics.Precision(name="precision"),
+        "recall": tf.keras.metrics.Recall(name="recall"),
+        "f1": tfa.metrics.F1Score(name="f1_score"),
+    }
+
+    result_subdir = utils.locate_result_subdir(run_id)
+    if log is not None:
+        log_file = os.path.join(result_subdir, log)
+    else:
+        log_file = os.path.join(result_subdir, "evaluation_log.txt")
+    print("Logging output to {}".format(log_file))
+
+    print("\nLoading the model best AUC model...")
+    model = tf.keras.models.load_model(os.path.join(result_subdir, "best_auc_model.h5"))
+    model.summary()
+
+    print("\nUsing the {} record to evaluate".format(test_record))
+    if config.train_record is not None:
+        test_dataset = tf.data.TFRecordDataset(test_record)
+    else:
+        raise Exception("No path for the Test TfRecord was given.")
+
+    # Apply the stack of transformations uncommented in config.py
+    test_dataset = dataset.map_functions(test_dataset, config.dataset.map_functions)
+
+    # Shuffle, batch and prefetch both datasets
+    test_batches = (
+        test_dataset.shuffle(config.dataset.shuffle, reshuffle_each_iteration=True)
+        .batch(config.dataset.batch_size)
+        .prefetch(config.dataset.prefetch)
+    )
+
+    # evaluate metrics
+    evaluation_metrics = model.evaluate(test_batches)
+    metrics = {
+        key: (round(value, 2) * 100)
+        for (key, value) in zip(metrics, ["loss"] + list(evaluation_metrics))
+    }
+
+    # append auc score
+    metrics["auc"] = {}
+    data = list(test_dataset.as_numpy_iterator())
+    x = np.asarray([element[0] for element in data])
+    y = np.asarray([element[1] for element in data])
+
+    y_hat = model.predict(x)
+
+    if class_names is None:
+        class_names = ["Class {}".format(i) for i in range(y.shape[1])]
+
+    current_auroc = []
+    for i in range(len(class_names)):
+        try:
+            score = roc_auc_score(y[:, i], y_hat[:, i])
+        except ValueError:
+            score = 0
+        metrics["auc"][class_names[i]] = round(score, 2) * 100
+        current_auroc.append(score)
+        print("{:02d}. {:26s} -> {:>8}".format(i + 1, class_names[i], score))
+
+    mean_auroc = np.mean(current_auroc)
+    metrics["auc"]["mean"] = round(mean_auroc, 2) * 100
+    print("--------------------------------------------------------")
+    print("MEAN AUROC: {:>40}".format(mean_auroc))
+    print("--------------------------------------------------------\n")
+
+    # save metrics with pickle
+    pickle_path = os.path.join(result_subdir, "metrics_on_evaluation.pkl")
+    print("Saving metrics to {}".format(pickle_path))
+    with open(pickle_path, "wb") as handle:
+        pickle.dump(metrics, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
 if __name__ == "__main__":
     utils.init_output_logging()
     tf.random.set_seed(config.random_seed)
@@ -122,6 +210,5 @@ if __name__ == "__main__":
     print("Initializing execution...")
     os.environ.update(config.env)
     print("Running %s()..." % config.train["func"])
-    result_subdir = utils.create_result_subdir(config.result_dir, config.desc)
-    utils.call_func_by_name(result_subdir=result_subdir, **config.train)
+    utils.call_func_by_name(**config.train)
     print("\nEnding execution...")
